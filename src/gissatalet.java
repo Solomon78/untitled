f37dd765ca94d8fd51a543f5c7707303d15fb872
Solomon Musie <solomontikabo.musie@learnet.se>
2019-10-18 11:17:57 +0200

    Initial commit

---
+++ src/gissatalet.java
@@ -0,0 +1,10 @@
+public class gissatalet {
+    public static void main(String[] args) {
+
+        System.out.println(" Gissa på en siffra:  ");
+      double Math
+        
+
+        if(guess > svar)
+    }
+}
